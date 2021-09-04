1b341f768b8b0e3a710cb0d045fcb6b28d55e583
phamtanphat <phatdroid94@gmail.com>
2021-09-04 20:13:41 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,31 @@
+public class Buoi1{
+    public static void main(String[]args){
+        // 1 : Kiểu dữ liệu
+        // comment code => đoạn này không thực thi (ctrl + / )
+        // String (Kiểu chuỗi) ,
+        // int ( kiểu số nguyên) ,
+        // long( giống số nguyên mà kích thước lớn hơn)ư
+        // float (kiểu số thực => số có phần thập phân 1.2)
+        // double (kiểu số thực nhưng kích thước lớn hơn kiểu float)
+        // boolean (kiểu luận lý tại vì chỉ có 2 giá trị là true hoặc false)
+
+        // type variable = value
+        String name = "Nguyễn Văn Tèo";
+        int number = 5;
+        long number2 = 10L;
+        float number3 = 1.5f;
+        double number4 = 1.5;
+        boolean isChecked = true;
+
+        // Hàm - phương thức
+        // ctrl + shilt + f10
+        System.out.println("Kiểu String " + name);
+        System.out.println("Kiểu int " + number);
+        System.out.println("Kiểu long " + number2);
+        System.out.println("Kiểu float " + number3);
+        System.out.println("Kiểu double " + number4);
+        System.out.println("Kiểu boolean " + isChecked);
+
+    }
+}
+
